c1193c687434255a3fb8d753f6d4bbd5139a9789
Bench-10 <145767536+Bench-10@users.noreply.github.com>
2025-07-17 16:03:56 +0800

    fix some bug

--- lib/features/pages/bookMark.dart
+++ lib/features/pages/bookMark.dart
@@ -26,7 +26,7 @@ class _MyAppState extends State<BookMarksPage> {
             )
           ),
 
-          SizedBox(height: 7,),
+          SizedBox(height: 8,),
 
           SizedBox(
             height: 30,
